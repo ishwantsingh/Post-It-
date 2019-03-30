cf4b1f0036a37d797d23984adeffbab8d6a95f75
Ishwant Singh <ishwant8@gmail.com>
2019-03-30 23:21:59 +0530

    style: user page

--- postit/src/components/user/User.js
+++ postit/src/components/user/User.js
@@ -3,20 +3,42 @@ import styled from "styled-components";
 import { Redirect } from "react-router-dom";
 
 const StyledImg = styled.img`
-  border-radius: 50%;
+  border-radius: 3%;
+  width: 70px;
+  height: 70px;
+`;
+const StyledDiv = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  .profile-pic-div {
+    display: flex;
+  }
+  h5 {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+  span {
+    font-size: 1.5rem;
+  }
 `;
 
 function User(props) {
   if (!props.auth.uid) return <Redirect to="/login" />;
   return (
-    <div>
-      <h3>{props.auth.displayName}</h3>
-      <StyledImg
-        src={props.auth.photoURL}
-        alt="profile pic"
-        className="profile-img"
-      />
-    </div>
+    <StyledDiv className="card-panel">
+      <h5>
+        Username: <span>{props.auth.displayName}</span>
+      </h5>
+      <div className="profile-pic-div">
+        <h5>Profile Picture:</h5>
+        <StyledImg
+          src={props.auth.photoURL}
+          alt="profile pic"
+          className="profile-img"
+        />
+      </div>
+    </StyledDiv>
   );
 }
 
